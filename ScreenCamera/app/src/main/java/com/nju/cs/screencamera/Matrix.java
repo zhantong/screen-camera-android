ae813eb52c784b2c3431159a8a8af05d5a76412e
zhantong1994 <zhantong1994@163.com>
2015-12-11 22:07:18 +0800

    迁移FindBoarder类到Matrix类，删除FindBoarder类
    由于FindBoarder类参数需要使用Matrix类，且FindBoarder类只用在Matrix类中，故删除FindBoarder类，由Ma
    trix类集成。虽然这样使得Matrix类变得臃肿，但对于后面用到其他颜色格式是有好处的。

--- ScreenCamera/app/src/main/java/com/nju/cs/screencamera/Matrix.java
+++ ScreenCamera/app/src/main/java/com/nju/cs/screencamera/Matrix.java
@@ -28,7 +28,7 @@ public final class Matrix {
         this.width=width;
         this.height=height;
         this.threshold=getThreshold();
-        this.borders=FindBoarder.findBoarder(this);
+        this.borders=findBoarder();
     }
 
     public void perspectiveTransform(float p1ToX, float p1ToY,
@@ -157,5 +157,149 @@ public final class Matrix {
         }
         if(VERBOSE){Log.d(TAG, "threshold:" + bestValley);}
         return bestValley;
+    }
+
+    public boolean containsBlack(int start,int end,int fixed,boolean horizontal){
+        if(horizontal){
+            for(int x=start;x<=end;x++){
+                if(pixelEquals(x, fixed, 0)){
+                    return true;
+                }
+
+            }
+        }
+        else{
+            for(int y=start;y<=end;y++){
+                if(pixelEquals(fixed, y, 0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public int[] findBoarder() throws NotFoundException{
+        int init=20;
+        int left=width/2-init;
+        int right=width/2+init;
+        int up=height/2-init;
+        int down=height/2+init;
+        int leftOrig=left;
+        int rightOrig=right;
+        int upOrig=up;
+        int downOrig=down;
+        if(VERBOSE){Log.d(TAG,"boarder init: up:"+up+"\t"+"right:"+right+"\t"+"down:"+down+"\t"+"left:"+left);}
+        if(left<0||right>=width||up<0||down>=height){
+            throw new NotFoundException("frame size too small");
+        }
+        boolean flag;
+        while(true){
+            flag=false;
+            while(containsBlack(up,down,right,false)&&right<width){
+                right++;
+                flag=true;
+
+            }
+            while(containsBlack(left,right,down,true)&&down<height){
+                down++;
+                flag=true;
+            }
+            while(containsBlack(up,down,left,false)&&left>0){
+                left--;
+                flag=true;
+            }
+            while(containsBlack(left,right,up,true)&&up>0){
+                up--;
+                flag=true;
+            }
+            if(!flag){
+                break;
+            }
+        }
+        if(VERBOSE){Log.d(TAG,"find boarder: up:"+up+"\t"+"right:"+right+"\t"+"down:"+down+"\t"+"left:"+left);}
+        if((left==0||up==0||right==width||down==height)||(left==leftOrig&&right==rightOrig&&up==upOrig&&down==downOrig)){
+            throw new NotFoundException("didn't find any possible bar code");
+        }
+        int[] vertexs=new int[8];
+        left=findVertex(up,down,left,leftOrig,vertexs,0,3,false,false);
+        if(VERBOSE){Log.d(TAG,"found 1 vertex");}
+        up=findVertex(left,right,up,upOrig,vertexs,0,1,true,false);
+        if(VERBOSE){Log.d(TAG,"found 2 vertex");}
+        right=findVertex(up,down,right,rightOrig,vertexs,1,2,false,true);
+        if(VERBOSE){Log.d(TAG,"found 3 vertex");}
+        down=findVertex(left,right,down,downOrig,vertexs,3,2,true,true);
+        if(VERBOSE){Log.d(TAG,"found 4 vertex");}
+        if(VERBOSE){
+            Log.d(TAG,"vertexes: ("+vertexs[0]+","+vertexs[1]+")\t("+vertexs[2]+","+vertexs[3]+")\t("+vertexs[4]+","+vertexs[5]+")\t("+vertexs[6]+","+vertexs[7]+")");
+        }
+        if(vertexs[0]==0||vertexs[2]==0||vertexs[4]==0||vertexs[6]==0){
+            throw new NotFoundException("vertexs error");
+        }
+        return vertexs;
+    }
+    public int findVertex(int b1,int b2,int fixed,int fixedOrig,int[] vertexs,int p1,int p2,boolean horizontal,boolean sub) throws NotFoundException{
+        int mid=(b2-b1)/2;
+        if(horizontal){
+            while(true){
+                for (int i = 1; i <= mid; i++) {
+                    if (pixelEquals(b1+i,fixed,0)) {
+                        if (!isSinglePoint(b1 + i, fixed)) {
+                            vertexs[p1 * 2] = b1 + i;
+                            vertexs[p1 * 2 + 1] = fixed;
+                            return fixed;
+                        }
+                    }
+                    if (pixelEquals(b2-i,fixed,0)) {
+                        if (!isSinglePoint(b2 - i, fixed)) {
+                            vertexs[p2 * 2] = b2 - i;
+                            vertexs[p2 * 2 + 1] = fixed;
+                            return fixed;
+                        }
+                    }
+                }
+                if(sub){
+                    fixed--;
+                    if(fixed<=fixedOrig){
+                        throw new NotFoundException("didn't find any possible bar code");
+                    }
+                }
+                else{
+                    fixed++;
+                    if(fixed>=fixedOrig){
+                        throw new NotFoundException("didn't find any possible bar code");
+                    }
+                }
+            }
+        }
+        else{
+            while(true) {
+                for (int i = 1; i <= mid; i++) {
+                    if (pixelEquals(fixed,b1+i,0)) {
+                        if (!isSinglePoint(fixed, b1 + i)) {
+                            vertexs[p1 * 2] = fixed;
+                            vertexs[p1 * 2 + 1] = b1 + i;
+                            return fixed;
+                        }
+                    }
+                    if (pixelEquals(fixed,b2-i,0)) {
+                        if (!isSinglePoint(fixed, b2 - i)) {
+                            vertexs[p2 * 2] = fixed;
+                            vertexs[p2 * 2 + 1] = b2 - i;
+                            return fixed;
+                        }
+                    }
+                }
+                if(sub){
+                    fixed--;
+                }
+                else{
+                    fixed++;
+                }
+            }
+        }
+    }
+    public boolean isSinglePoint(int x,int y){
+        int sum= get(x-1,y-1)+ get(x,y-1)+ get(x+1,y-1)+ get(x-1,y)+ get(x+1,y)+ get(x-1,y+1)+ get(x,y+1)+ get(x+1,y+1);
+        //System.out.println("isSinglePoint:"+sum);
+        return sum>=6;
     }
 }
