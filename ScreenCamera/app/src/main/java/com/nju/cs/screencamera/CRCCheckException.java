c1359b145fe5cb299a5c188d8daea0865213d77f
zhantong1994 <zhantong1994@163.com>
2015-12-12 17:23:29 +0800

    CRCCheckException类添加注释，整理格式

--- ScreenCamera/app/src/main/java/com/nju/cs/screencamera/CRCCheckException.java
+++ ScreenCamera/app/src/main/java/com/nju/cs/screencamera/CRCCheckException.java
@@ -1,17 +1,21 @@
 package com.nju.cs.screencamera;
 
 /**
- * Created by zhantong on 15/12/11.
+ * CRC校验异常类
+ * CRC校验值不匹配时抛出此异常
  */
-public class CRCCheckException extends Exception{
-    private static final CRCCheckException INSTANCE=new CRCCheckException();
-    protected static final StackTraceElement[] NO_TRACE=new StackTraceElement[0];
+public class CRCCheckException extends Exception {
+    private static final CRCCheckException INSTANCE = new CRCCheckException();
+    protected static final StackTraceElement[] NO_TRACE = new StackTraceElement[0];
+
     static {
         INSTANCE.setStackTrace(NO_TRACE);
     }
-    private CRCCheckException(){
+
+    private CRCCheckException() {
     }
-    public static CRCCheckException getNotFoundInstance(){
+
+    public static CRCCheckException getNotFoundInstance() {
         return INSTANCE;
     }
 }
