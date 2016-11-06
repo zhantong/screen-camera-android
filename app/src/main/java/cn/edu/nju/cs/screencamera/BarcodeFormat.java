50a83dc1f87857bd19ff5df2c984798ac392f81a
zhantong <zhantong1994@163.com>
2016-11-06 20:09:22 +0800

    加入新的二维码ZoomVaryAltBar 在ZoomVaryAltBar的基础上增加了两对渐变条。

--- app/src/main/java/cn/edu/nju/cs/screencamera/BarcodeFormat.java
+++ app/src/main/java/cn/edu/nju/cs/screencamera/BarcodeFormat.java
@@ -7,7 +7,8 @@ public enum BarcodeFormat{
     NORMAL("普通二维码"),
     ZOOM("小方块移动二维码"),
     ZOOMVARY("黑白移动"),
-    ZOOMVARYALT("黑白交替移动");
+    ZOOMVARYALT("黑白交替移动"),
+    ZOOMVARYALTBAR("黑白交替移动渐变条");
     private String friendlyName;
     private BarcodeFormat(String friendlyName){
         this.friendlyName=friendlyName;
