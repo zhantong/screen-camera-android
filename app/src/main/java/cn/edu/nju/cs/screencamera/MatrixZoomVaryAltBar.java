85f5a8bfe3259089c68ea88bd5dd8569d9feb04f
zhantong <zhantong1994@163.com>
2016-11-08 22:17:48 +0800

    读取参考条，并保存到log 参考条记录从开始到结尾的每个原始像素值，以行为Key。将参考条数据单独保存是为减小log文件起见。

--- app/src/main/java/cn/edu/nju/cs/screencamera/MatrixZoomVaryAltBar.java
+++ app/src/main/java/cn/edu/nju/cs/screencamera/MatrixZoomVaryAltBar.java
@@ -1,8 +1,18 @@
 package cn.edu.nju.cs.screencamera;
 
+import android.util.ArrayMap;
+import android.util.Pair;
 import android.util.SparseIntArray;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.IntNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -43,17 +53,43 @@ public class MatrixZoomVaryAltBar extends MatrixZoomVaryAlt{
     }
     public void initGrayMatrix(int dimensionX, int dimensionY){
         super.initGrayMatrix(dimensionX,dimensionY);
-        int x=frameBlackLength;
-        for(int y=frameBlackLength;y<frameBlackLength+contentLength;y++){
-            Point[] points=grayMatrix.getPoints(x,y);
-            points[0].print();
+    }
+    public JsonNode getSampleDataInJSON(){
+        JsonNode root=super.getSampleDataInJSON();
+
+        SparseIntArray varyOne=new SparseIntArray();
+        scanVaryBar(frameBlackLength,varyOne);
+        scanVaryBar(frameBlackLength+frameVaryLength+frameVaryTwoLength+contentLength,varyOne);
+        SparseIntArray varyTwo=new SparseIntArray();
+        scanVaryBar(frameBlackLength+frameVaryLength,varyTwo);
+        scanVaryBar(frameBlackLength+frameVaryLength+frameVaryTwoLength+contentLength+frameVaryLength,varyTwo);
+
+        ObjectMapper mapper=new ObjectMapper();
+        JsonNode reference=mapper.createObjectNode();
+        ((ObjectNode)reference).set("referenceColorOne",sparseArrayToJSON(varyOne,mapper));
+        ((ObjectNode)reference).set("referenceColorTwo",sparseArrayToJSON(varyTwo,mapper));
+        ((ObjectNode)root).set("reference",reference);
+        return root;
+    }
+    private static JsonNode sparseArrayToJSON(SparseIntArray sparseIntArray,ObjectMapper mapper){
+        JsonNode root=mapper.createObjectNode();
+        ArrayNode keys=mapper.createArrayNode();
+        ArrayNode values=mapper.createArrayNode();
+        for(int i=0;i<sparseIntArray.size();i++){
+            keys.add(IntNode.valueOf(sparseIntArray.keyAt(i)));
+            values.add(IntNode.valueOf(sparseIntArray.valueAt(i)));
         }
-        SparseIntArray line=scanColumn(frameBlackLength,frameBlackLength,frameBlackLength+contentLength);
-        for(int i=0;i<line.size();i++){
-            int key=line.keyAt(i);
-            int value=line.get(key);
-            int pixel=getGray(key,value);
-            System.out.println("out:"+key+" "+value+" "+pixel);
+        ((ObjectNode)root).set("keys",keys);
+        ((ObjectNode)root).set("values",values);
+        return root;
+    }
+    private void scanVaryBar(int columnX,SparseIntArray map){
+        SparseIntArray column=scanColumn(columnX,frameBlackLength,frameBlackLength+contentLength);
+        for(int i=0;i<column.size();i++){
+            int y=column.keyAt(i);
+            int x=column.get(y);
+            int grayValue=getGray(x,y);
+            map.put(y,grayValue);
         }
     }
     private SparseIntArray scanColumn(int x,int yStart,int yEnd){
@@ -65,17 +101,15 @@ public class MatrixZoomVaryAltBar extends MatrixZoomVaryAlt{
             int yPrev=pointPrev.y;
             int xNext=pointNext.x;
             int yNext=pointNext.y;
-            SparseIntArray line=findLine(xPrev,yPrev,xNext,yNext);
-            for(int i=0;i<line.size();i++){
-                int key=line.keyAt(i);
-                int value=line.get(key);
-                array.put(key,value);
+            List<Pair> line=findLine(xPrev,yPrev,xNext,yNext);
+            for(Pair pair:line){
+                array.put((int)pair.second,(int)pair.first);
             }
         }
         return array;
     }
-    private SparseIntArray findLine(int x0,int y0,int x1,int y1){
-        SparseIntArray line=new SparseIntArray();
+    private List<Pair> findLine(int x0,int y0,int x1,int y1){
+        List<Pair> line=new ArrayList<>();
         int dx=Math.abs(x1-x0);
         int dy=Math.abs(y1-y0);
 
@@ -88,7 +122,7 @@ public class MatrixZoomVaryAltBar extends MatrixZoomVaryAlt{
         int currentY=y0;
 
         while(true){
-            line.append(currentX,currentY);
+            line.add(new Pair(currentX,currentY));
 
             if(currentX==x1&&currentY==y1){
                 break;
