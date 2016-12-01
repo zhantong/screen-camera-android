6b467f013b49b25361ab9cc3c5f856a0db6d1bfc
Tong ZHAN <zhantong1994@163.com>
2016-12-01 08:35:20 +0800

    Zone增加Json导出，增加列扫描

--- app/src/main/java/cn/edu/nju/cs/screencamera/Zone.java
+++ app/src/main/java/cn/edu/nju/cs/screencamera/Zone.java
@@ -1,7 +1,14 @@
 package cn.edu.nju.cs.screencamera;
 
+import android.util.Pair;
+import android.util.SparseIntArray;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 import java.util.Arrays;
 import java.util.BitSet;
+import java.util.List;
 
 /**
  * Created by zhantong on 2016/11/24.
@@ -15,7 +22,7 @@ public class Zone {
     private int[] content;
     private Block block;
     private float[] standardSamplePoints=null;
-    private float[] realSamplePoints=null;
+    private int[] realSamplePoints=null;
     public Zone(int widthInBlock, int heightInBlock,int baseOffsetInBlockX,int baseOffsetInBlockY){
         this(widthInBlock,heightInBlock,baseOffsetInBlockX,baseOffsetInBlockY,null);
     }
@@ -53,27 +60,54 @@ public class Zone {
         }
         return standardSamplePoints;
     }
-    public float[] getRealSamplePoints(PerspectiveTransform transform){
+    public int[] getRealSamplePoints(PerspectiveTransform transform){
         if(realSamplePoints==null){
             float[] standard=getStandardSamplePoints();
-            realSamplePoints=Arrays.copyOf(standard,standard.length);
-            transform.transformPoints(realSamplePoints);
+            float[] real=Arrays.copyOf(standard,standard.length);
+            transform.transformPoints(real);
+            realSamplePoints=new int[real.length];
+            for(int i=0;i<real.length;i++){
+                realSamplePoints[i]=Math.round(real[i]);
+            }
         }
         return realSamplePoints;
     }
     public int[] getContent(PerspectiveTransform transform,RawImage rawImage){
         if(content==null){
-            float[] real=getRealSamplePoints(transform);
+            int[] real=getRealSamplePoints(transform);
             int numBlockSamplePoints=block.getNumSamplePoints();
             content=new int[widthInBlock*heightInBlock*numBlockSamplePoints];
             for(int i=0,pos=0;i<real.length;i+=2,pos++){
-                int x=(int)real[i];
-                int y=(int)real[i+1];
+                int x=real[i];
+                int y=real[i+1];
                 int value=rawImage.getGray(x,y);
                 content[pos]=value;
             }
         }
         return content;
+    }
+    public void scanColumn(int x,SparseIntArray map,PerspectiveTransform transform,RawImage rawImage){
+        for(int y=0;y<heightInBlock-1;y++){
+            float standardXPrev=baseOffsetInBlockX+x+0.5f;
+            float standardYPrev=baseOffsetInBlockY+y+0.5f;
+            float[] realCoodPrev=transform.transformPoint(standardXPrev,standardYPrev);
+            int realXPrev=Math.round(realCoodPrev[0]);
+            int realYPrev=Math.round(realCoodPrev[1]);
+
+            float standardXNext=baseOffsetInBlockX+x+0.5f;
+            float standardYNext=baseOffsetInBlockY+y+0.5f+1;
+            float[] realCoodNext=transform.transformPoint(standardXNext,standardYNext);
+            int realXNext=Math.round(realCoodNext[0]);
+            int realYNext=Math.round(realCoodNext[1]);
+
+            List<Pair> line=Utils.findLine(realXPrev,realYPrev,realXNext,realYNext);
+            for(Pair<Integer,Integer> pair:line){
+                int column=pair.first;
+                int row=pair.second;
+                int grayValue=rawImage.getGray(column,row);
+                map.put(row,grayValue);
+            }
+        }
     }
     public int startInBlockX(){
         return baseOffsetInBlockX;
@@ -86,5 +120,13 @@ public class Zone {
     }
     public int endInBlockY(){
         return baseOffsetInBlockY+heightInBlock;
+    }
+    public JsonObject toJson(){
+        Gson gson=new Gson();
+        JsonObject root= new JsonObject();
+        root.addProperty("samplesPerUnit",block.getNumSamplePoints());
+        root.add("real",gson.toJsonTree(realSamplePoints));
+        root.add("content",gson.toJsonTree(content));
+        return root;
     }
 }
