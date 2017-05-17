4f54879136475c79c45b86d2a43027d36579a2f1
Tong ZHAN <zhantong1994@163.com>
2017-05-17 13:44:56 +0800

    加入ColorBlock

---
+++ app/src/main/java/cn/edu/nju/cs/screencamera/ColorBlock.java
@@ -0,0 +1,27 @@
+package cn.edu.nju.cs.screencamera;
+
+/**
+ * Created by zhantong on 2017/5/15.
+ */
+
+public class ColorBlock implements Block {
+    private float[] samplePoints=new float[]{0.5f,0.5f};
+    private int bitsPerUnit;
+    public ColorBlock(int bitsPerUnit){
+        this.bitsPerUnit=bitsPerUnit;
+    }
+    public int getBitsPerUnit() {
+        return bitsPerUnit;
+    }
+    public float[] getSamplePoints(){
+        return samplePoints;
+    }
+    public int getNumSamplePoints(){
+        return samplePoints.length/2;
+    }
+
+    @Override
+    public int[] getChannels() {
+        return null;
+    }
+}
