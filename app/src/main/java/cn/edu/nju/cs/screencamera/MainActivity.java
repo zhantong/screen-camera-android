00c405c21ed7d30caf32ed2a731725b9d3b1910c
Tong ZHAN <zhantong1994@163.com>
2017-12-07 21:15:28 +0800

    增加将配置文件从Assets复制到内部存储功能

--- app/src/main/java/cn/edu/nju/cs/screencamera/MainActivity.java
+++ app/src/main/java/cn/edu/nju/cs/screencamera/MainActivity.java
@@ -2,12 +2,19 @@ package cn.edu.nju.cs.screencamera;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Created by zhantong on 2017/11/14.
@@ -35,6 +42,8 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        copyConfigsFromAssetsToStorage();
     }
 
     @Override
@@ -58,5 +67,34 @@ public class MainActivity extends Activity {
     void showSettings() {
         Intent intent = new Intent(this, MainSettingsActivity.class);
         startActivity(intent);
+    }
+
+    void copyConfigsFromAssetsToStorage() {
+        String CONFIG_FLODER_NAME = "configs";
+        AssetManager assetManager = getAssets();
+        String[] configFileNames = new String[0];
+        try {
+            configFileNames = assetManager.list(CONFIG_FLODER_NAME);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File parentPath = new File(getFilesDir(), CONFIG_FLODER_NAME);
+        if (!parentPath.exists()) {
+            parentPath.mkdir();
+        }
+        for (String configFileName : configFileNames) {
+            try {
+                InputStream in = assetManager.open(CONFIG_FLODER_NAME + "/" + configFileName);
+                OutputStream out = new FileOutputStream(new File(parentPath, configFileName));
+                byte[] buffer = new byte[1024];
+                int read = in.read(buffer);
+                while (read != -1) {
+                    out.write(buffer, 0, read);
+                    read = in.read(buffer);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
